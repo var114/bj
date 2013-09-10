b0933850a250922d78130c86cd4e86063a87bdbf
Patil Varvarian <Patil.Varvarian@gmail.com>
2013-09-10 10:10:26 -0700

    Episode1

---
+++ lib/bj.rb
@@ -0,0 +1,150 @@
+require 'rspec'
+class Card
+
+    attr_reader :suit, :value
+
+    def initialize(suit, value)
+	     @suit = suit
+	     @value = value
+    end
+
+    def value
+	    return 10 if ["J", "Q", "K"].include?(@value)
+	    return 11 if @value == "A"
+		return @value
+	end
+
+	def to_s
+		"#{@value}-#{suit}"
+	end
+end
+
+class Deck 
+
+	attr_reader :cards
+
+	def initialize
+		@cards = Deck.build_cards
+	end
+
+	def self.build_cards
+		cards = [] #just a local variable
+		[:clubs, :spades, :hearts, :diamonds].each do |suit|
+		(2..10).each do |number|
+			cards << Card.new(suit, number)
+		end
+		["J", "Q", "Q", "A"].each do |facecard|
+			cards << Card.new(suit, facecard)
+	    end
+	 end
+		cards.shuffle
+    end
+end
+
+class Hand 
+
+	attr_reader :cards
+
+	def initialize
+		@cards = []
+	end
+	def hit!(deck)
+		@cards << deck.cards.shift
+	end
+
+	def value #calling value method-in card
+		cards.inject(0) { |sum, card| sum += card.value }		
+	end
+
+	def play_as_dealer(deck)		
+		if value < 16
+			hit!(deck)
+			play_as_dealer(deck)
+		end
+	end 
+
+
+    def secret_cards #fix
+        cards.each do |card|
+        	card == "X"
+        	puts "frog"
+        end
+    end
+
+end
+
+class Game
+
+	attr_reader :player_hand , :dealer_hand
+
+	def initialize
+		 @deck = Deck.new
+		 @player_hand = Hand.new
+		 @dealer_hand = Hand.new
+		 2.times { @player_hand.hit!(@deck) }
+		 2.times { @dealer_hand.hit!(@deck) }	     
+	end
+
+	def hit	 #ass#1.2
+			if player_hand.value > 21
+				stand
+		elsif player_hand.value < 21  
+				@player_hand.hit!(@deck)
+				standfor 
+		elsif player_hand.value == 21
+				stand
+			end
+	end
+
+	def standfor #ass#1.2
+		if player_hand.value > 21
+			stand
+		else
+			@player_hand.cards
+		end
+	end
+
+	def player_status
+	  {	:player_card => @player_hand.cards,
+		:player_value => @player_hand.value }
+	end
+	def status
+	{
+		:player_card => @player_hand.cards,
+		:player_value => @player_hand.value,
+		:dealer_card => @dealer_hand.cards,
+		:dealer_value => @dealer_hand.value,
+		:winner => determine_winner(@player_hand.value, @dealer_hand.value)
+									}											
+	end
+
+	def dealer_status
+		@dealer_hand.secret_cards
+	end
+
+	def stand
+		@dealer_hand.play_as_dealer(@deck)
+		determine_winner(@player_hand.value, @dealer_hand.value)
+	end	
+
+	def determine_winner(player_value, dealer_value) 
+		if player_value > 21
+			:dealer
+	elsif dealer_value > 21
+			:player
+	elsif player_value > dealer_value
+			:player
+	elsif player_value == dealer_value
+			:push
+		else
+			:dealer
+	    end
+	end	
+
+	def inspect
+		status
+	end
+
+end
+
+
